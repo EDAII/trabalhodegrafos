a13882cdf7c977b2e600ea38de274cb58545047f
emanueng <emanuelhbga@gmail.com>
2018-11-21 19:45:49 -0200

    resolvendo problema na travessia em largura

--- grafo.py
+++ grafo.py
@@ -14,27 +14,36 @@ class No:
           for i in range(n):
             a=randint(1,100)
             self.nlista.append(No(a))
-      def printatipo(self): 
-          n2lista=[]   
-          for n in self.nlista:
-           if(type(n)!=int):
-              if(n.bool2==True):
-                return 
-              if(n.bool2==False):   
+      def travesialargura(self): 
+          n2lista=[]
+          n2lista.append(self) 
+          for n in n2lista:
+             if(self.isunknow(n)):     
+                n2lista.append(n)
                 for n2 in n.nlista:
-                     n2lista.append(n2)
-                n.bool2=True
-              print(n.retornatamanho())           
+                        if(self.isunknow(n2)):
+                              n2lista.append(n2)
+                              print("a")  
+             n2lista.remove(n)
+             n.bool2=True   
+                       
+          if(len(n2lista)==0):
+             return       
                                 
       def  retornatamanho(self):
            self.bool2=True  
-           return len(self.nlista)  
+           return len(self.nlista) 
+      def isunknow(self,a):
+           if(type(a)!=int):
+             if(a.bool2==False):
+               return True
+             return False         
 def main():
    a= input("digite um numero")
    b=int(a)
    p1=No(b)  
    p1.criavizinhos(b)
-   p1.printatipo()  
+   p1.travesialargura()  
    p3=p1.getno(2)    
    print("o tamanho e:", p3.retornatamanho())        
 
