cd87538560f53f711d0a7ab59f1c30e295ceb604
emanueng <emanuelhbga@gmail.com>
2018-11-22 15:10:08 -0200

    consertando travesia em largura

--- grafo.py
+++ grafo.py
@@ -1,10 +1,11 @@
 from random import randint
 class No:
       nlista=[]
-      bool2=False  
+      bool2=False 
+      gosto=[] 
       def __init__(self,n):
         self.nlista.append(n)
-        self.bool2=False  
+        self.bool2=False     
 
       def addno(self, n):
           self.nlista.append(n)
@@ -13,38 +14,41 @@ class No:
       def criavizinhos(self, n ):
           for i in range(n):
             a=randint(1,100)
+            print(a)  
             self.nlista.append(No(a))
       def travesialargura(self): 
           n2lista=[]
           n2lista.append(self) 
           for n in n2lista:
-             if(self.isunknow(n)):     
-                n2lista.append(n)
+              # contador de visita bem aqui. 
+             if(self.isunknow(n)):
+                print("b")
+                print(n)    
+                n.bool2=True      
                 for n2 in n.nlista:
                         if(self.isunknow(n2)):
                               n2lista.append(n2)
-                              print("a")  
-             n2lista.remove(n)
-             n.bool2=True   
-                       
-          if(len(n2lista)==0):
-             return       
-                                
+                              print("a")                   
+         
+                           
       def  retornatamanho(self):
            self.bool2=True  
            return len(self.nlista) 
+
       def isunknow(self,a):
-           if(type(a)!=int):
+           if(type(a)==No):
              if(a.bool2==False):
                return True
-             return False         
+             else: 
+               return False
+           else:
+              return False         
 def main():
    a= input("digite um numero")
    b=int(a)
-   p1=No(b)  
+   p1=No(b)     
    p1.criavizinhos(b)
-   p1.travesialargura()  
-   p3=p1.getno(2)    
-   print("o tamanho e:", p3.retornatamanho())        
+   p1.travesialargura()   
+   print("o tamanho e:", p1.retornatamanho())        
 
 main()
